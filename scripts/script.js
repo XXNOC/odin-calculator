17debaca62a37456abf28a19a5a54a0f5db14f3b
XNOC <99592950+XXNOC@users.noreply.github.com>
2023-09-05 02:08:07 +0300

    Added basic calculator

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,31 @@
 "use strict";
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function operate(str) {
+    let possibleOps = ["+", "-", "*", "/"];
+    let strArr = str.split(" ");
+    let a = parseInt(strArr[0]);
+    let op = strArr[1];
+    let b = parseInt(strArr[2]);
+    if (!possibleOps.includes(op) || isNaN(a) || isNaN(b))
+        return NaN;
+    if (op === "+")
+        return add(a, b);
+    if (op === "-")
+        return subtract(a, b);
+    if (op === "*")
+        return multiply(a, b);
+    if (op === "/")
+        return divide(a, b);
+    return NaN;
+}
